228fbed4d59ef252a01f66971ac37dea15904a05
Satyam192 <108665611+Satyam192@users.noreply.github.com>
2022-07-14 22:35:46 +0530

    Add files via upload

---
+++ Github_stack_and_Queue.py
@@ -0,0 +1,64 @@
+#Queue Implementation
+
+# Python program to
+# demonstrate queue implementation
+# using list
+
+# Initializing a queue
+queue = []
+
+# Adding elements to the queue
+queue.append('a')
+queue.append('b')
+queue.append('c')
+
+print("Initial queue")
+print(queue)
+
+#Remove Element in Queue
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)
+
+
+
+
+
+
+
+#Stack Implementation
+
+
+# Python program to
+# demonstrate stack implementation
+# using list
+
+stack = []
+
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial stack')
+print(stack)
+
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
+
+
+
+
